baddeaf1c94e6aec136dafa12c3d6dee8a630524
evanwrench <ewrench@students.cs.ubc.ca>
2022-06-02 22:52:00 -0700

    Icon changes

--- frontend/src/components/ChampionIcon.js
+++ frontend/src/components/ChampionIcon.js
@@ -1,13 +1,8 @@
 const ChampionIcon = (name, icon) => {
   return (
     <div>
-        <Image
-        radius="md"
-        alt="Random unsplash image"
-        //src = icon
-        withPlaceholder
-      />
-      <Text size="sm">name</Text>
+      <img src={icon} alt="" />
+      <p>{name}</p>
     </div>
   )
 }
