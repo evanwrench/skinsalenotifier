1237d7cfabdef69026789aa1cec890d276d271d7
evanwrench <ewrench@students.cs.ubc.ca>
2022-06-02 22:39:41 -0700

    added search and icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Testing from './components/Testing'
+import ChampionSearch from './components/ChampionSearch'
 import { MantineProvider } from '@mantine/core';
 
 function App() {
   return (
-    <Testing />
+    <div>
+      <Testing />
+      <ChampionSearch />
+    </div>
+    
   );
 }
 
