072f9ebb471853a5b3f02166503d57b4a7f40e12
evanwrench <ewrench@students.cs.ubc.ca>
2022-06-05 18:05:36 -0700

    Added example images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import Testing from './components/Testing'
 import ChampionSearch from './components/ChampionSearch'
-import { Space } from '@mantine/core';
+import { Grid, Space } from '@mantine/core';
 import ChampionIcon from './components/ChampionIcon';
+import bard from './images/BardSquare.webp'
+import aphe from './images/ApheliosSquare.webp'
+import reng from './images/RengarSquare.webp'
+import velk from './images/VelKozSquare.webp'
+// Defenitely an easier way to import icons ^^^
 
 function App() {
   return (
@@ -11,10 +16,24 @@ function App() {
       <Space h="xl"/>
       <ChampionSearch />
       <Space h="xl"/>
-      <ChampionIcon name="Champion" icon=""/>
+      <Grid justify = "space-around">
+      <Grid.Col span={1}>
+          <ChampionIcon name="Aphelios" icon={aphe}/>
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <ChampionIcon name="Bard" icon={bard}/>
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <ChampionIcon name="Rengar" icon={reng}/>
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <ChampionIcon name="Velkoz" icon={velk}/>
+        </Grid.Col>
+      </Grid>
     </div>
     
   );
 }
+
 
 export default App;
