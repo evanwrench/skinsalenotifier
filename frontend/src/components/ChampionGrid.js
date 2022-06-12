0834f4a8395695a8f6ec8705e3ea3e08c969d396
Cyrus Kalafchi <cyrus.kalafchi@icloud.com>
2022-06-12 14:08:06 -0700

    useEffect + useState demo

--- frontend/src/components/ChampionGrid.js
+++ frontend/src/components/ChampionGrid.js
@@ -3,11 +3,21 @@ import bard from '../images/BardSquare.webp'
 import aphe from '../images/ApheliosSquare.webp'
 import reng from '../images/RengarSquare.webp'
 import velk from '../images/VelKozSquare.webp'
-import { Grid } from '@mantine/core';
+import { Grid, Button } from '@mantine/core';
+import { useEffect, useState } from 'react';
 
 const ChampionGrid = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Hello World");
+  }, [count]);
+
   return (
     <Grid justify = "space-around">
+      <Button onClick={() => setCount(count + 1)}>
+        {count}
+      </Button>
       <Grid.Col span={1}>
           <ChampionIcon name="Aphelios" icon={aphe}/>
         </Grid.Col>
