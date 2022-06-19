03c8144750f912902d54eeda633afed876c4d1cd
evanwrench <ewrench@students.cs.ubc.ca>
2022-06-19 13:54:48 -0700

    Added link to icons

--- frontend/src/components/ChampionGrid.js
+++ frontend/src/components/ChampionGrid.js
@@ -2,6 +2,7 @@ import ChampionIcon from './ChampionIcon';
 import { Grid, Button } from '@mantine/core';
 import { useEffect, useState } from 'react';
 import bard from '../images/BardSquare.webp'
+import { Link } from 'react-router-dom';
 
 const ChampionGrid = () => {
   const JsonData = require('../champion.json');
@@ -15,7 +16,7 @@ const ChampionGrid = () => {
   return (
     <Grid>
       {ListData.map( (item, i) => 
-      <Grid.Col span={2}><ChampionIcon name = {item[1]["name"]} icon = {bard} /> </Grid.Col>)};
+      <Grid.Col span={2}><Link to="/champview"><ChampionIcon name = {item[1]["name"]} icon = {bard} /></Link> </Grid.Col>)};
       {/* ICON: {"../assets/champion-tiles/" + item[1]["image"]["full"]}*/ }
       
       </Grid>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,14 +2,11 @@ import Testing from '../components/Testing'
 import ChampionSearch from '../components/ChampionSearch'
 import { Space } from '@mantine/core';
 import ChampionGrid from '../components/ChampionGrid'
-import ChampView from './ChampView';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
 function Home() {
   return (
     <div>
         <Testing />
-        <Link to="/champview">TEST</Link>
         <Space h="xl"/>
         <ChampionSearch />
         <Space h="xl"/>
