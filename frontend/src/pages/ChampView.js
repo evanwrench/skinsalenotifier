a1daa5023f4518495a5ffd0ac191698b7b8b0c27
evanwrench <ewrench@students.cs.ubc.ca>
2022-07-10 18:26:44 -0700

    Refactoring and conversion fn

--- frontend/src/pages/ChampView.js
+++ frontend/src/pages/ChampView.js
@@ -1,9 +1,33 @@
-
+import { useSearchParams } from "react-router-dom"
 
 const ChampView = () => {
+  let [ searchParams,setSearchParams ] = useSearchParams();
+  var champName = searchParams.get('champ');
+  var champJsonName = convertToJsonName(champName);
+
+  // Match champ name with json file name
+  function convertToJsonName(s) {
+    let space = true;
+    let result = "";
+    for (let i = 0; i < s.length; i++) {
+      let c = s.charAt(i);
+      if (c === '\'' || c === '.') {
+        // Do nothing
+      } else if (c === " ") {
+        space = true;
+      } else if (space) {
+        result += c.toUpperCase();
+        space = false;
+      } else {
+        result += c.toLowerCase();
+      }
+    }
+    return result;
+  }
+
   return (
     <div>
-        <h1>Champ View</h1>
+        <h1>{champJsonName}</h1>
     </div>
   )
 }
